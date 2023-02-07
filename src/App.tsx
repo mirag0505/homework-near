3f360ed46c4ddd3d5afb62df36c0d692bc5ea736
AlexDi <mirag0505@yandex.ru>
2023-02-07 22:58:01 +0800

    adedd signOut, signIn methods and show extra data

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import * as nearAPI from "near-api-js";
+import {WalletConnection} from "near-api-js";
+import {useEffect, useState} from "react";
+import {AccountBalance} from "near-api-js/lib/account";
+
+const CONTRACT_ADDRESS = 'homeworknear.testnet';
+const {connect, keyStores} = nearAPI;
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [walletConnection, setWalletConnection] = useState<WalletConnection>()
+    const [balance, setBalance] = useState<AccountBalance | undefined>()
+    const [accountName, setAccountName] = useState<string | undefined>()
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/public/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+
+    const connectionConfig = {
+        networkId: "testnet",
+        keyStore: new keyStores.BrowserLocalStorageKeyStore(), // first create a key store
+        nodeUrl: "https://rpc.testnet.near.org",
+        walletUrl: "https://wallet.testnet.near.org",
+        helperUrl: "https://helper.testnet.near.org",
+        explorerUrl: "https://explorer.testnet.near.org",
+    };
+
+    const getWalletConnection = async () => {
+        const nearConnection = await connect(connectionConfig);
+        // create wallet connection
+        const walletConnection = new WalletConnection(nearConnection, 'my-app-homework');
+
+        setWalletConnection(walletConnection)
+
+        // gets account balance
+        const account = await walletConnection?.account();
+
+        setBalance(await account?.getAccountBalance())
+        setAccountName(walletConnection?.getAccountId())
+    }
+
+    useEffect(() => {
+        getWalletConnection()
+    }, [])
+
+    useEffect(() => {
+        walletConnection?.isSignedIn() ? setIsLoggedIn(true) : setIsLoggedIn(false)
+    }, [walletConnection])
+
+    const signIn = async () => {
+        console.log('signIn')
+        // connect to NEAR
+        walletConnection?.requestSignIn({
+            contractId: CONTRACT_ADDRESS,
+        });
+    }
+
+    const signOut = () => {
+        console.log('signOut')
+        walletConnection?.signOut();
+        setIsLoggedIn(false)
+    }
+
+
+    return (
+        <div className="App">
+            <h1>
+                {isLoggedIn ? "you are logged in" : "you are not logged in"}
+            </h1>
+            <div>
+                <p>balance: {!balance ? 'loading...' : <pre>{JSON.stringify(balance)}</pre>}</p>
+            </div>
+            <div>accountName: {!accountName ? 'loading...' : <pre>{JSON.stringify(accountName)}</pre>}</div>
+            <div className="card">
+                {
+                    isLoggedIn ? <button onClick={() => signOut()}>
+                        signOut Wallet
+                    </button> : <button onClick={() => signIn()}>
+                        signIn Wallet
+                    </button>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// https://github.com/sodatea/vite-plugin-node-stdlib-browser
+import nodePolyfills from 'vite-plugin-node-stdlib-browser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [nodePolyfills(), react()],
 })
